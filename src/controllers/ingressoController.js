cd3d1eef1b4fea91187144af47cda332aa822dc5
anna leme <162629023+annabialeme@users.noreply.github.com>
2025-03-18 11:46:35 -0300

    fix: word controller

--- src/controllers/ingressoController.js
+++ src/controllers/ingressoController.js
@@ -2,7 +2,7 @@ const ingressoModel = require("../models/ingressoModel");
 
 const getAllIngressos = async (req, res) => {
     try {
-        const ingressos = await ingressoModel.getIngressos();
+        const ingressos = await ingressoModel.getIngresso();
         res.json(ingressos);
     } catch (error) {
         res.status(404).json({ message: "Erro ao buscar ingressos." });
--- src/models/ingressoModel.js
+++ src/models/ingressoModel.js
@@ -1,6 +1,6 @@
 const pool = require("../config/database");
 
-const getIngressos = async () => {
+const getIngresso = async () => {
     const result = await pool.query("SELECT * FROM ingressos");
     return result.rows;
 };
@@ -12,7 +12,7 @@ const getIngressoById = async (id) => {
 
 const createIngresso = async (evento, local, data_evento, categoria, preco, quantidade_disponivel) => {
     const result = await pool.query(
-        "INSERT INTO ingressos (evento, local, data_evento, categoria, preco, quantidade_disponivel) VALUES ($1, $2) RETURNING *",
+        "INSERT INTO ingressos (evento, local, data_evento, categoria, preco, quantidade_disponivel) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
         [evento, local, data_evento, categoria, preco, quantidade_disponivel]
     );
     return result.rows[0];
@@ -36,4 +36,4 @@ const deleteIngresso = async (id) => {
     return { message: "Ingresso deletado com sucesso." };
 };
 
-module.exports = { getIngressos, getIngressoById, createIngresso, updateIngresso, deleteIngresso };
+module.exports = { getIngresso, getIngressoById, createIngresso, updateIngresso, deleteIngresso };
