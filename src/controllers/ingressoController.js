a1386ecbe06b777e42ceb68bb94ca0a36c7531c4
anna leme <162629023+annabialeme@users.noreply.github.com>
2025-03-18 09:28:53 -0300

    feat: Controller ingresso

---
+++ src/controllers/ingressoController.js
@@ -0,0 +1,61 @@
+const ingressoModel = require("../models/ingressoModel");
+
+const getAllIngressos = async (req, res) => {
+    try {
+        const ingressos = await ingressoModel.getIngressos();
+        res.json(ingressos);
+    } catch (error) {
+        res.status(404).json({ message: "Erro ao buscar ingressos." });
+    }
+};
+
+const getIngresso = async (req, res) => {
+    try {
+        const ingresso = await ingressoModel.getIngressoById(req.params.id);
+        if (!ingresso) {
+            return res.status(404).json({ message: "Ingresso não encontrado." });
+        }
+        res.json(ingresso);
+    } catch (error) {
+        res.status(500).json({ message: "Erro ao buscar Ingresso." });
+    }
+};
+
+const createIngresso = async (req, res) => {
+    try {
+        const { evento, local, data_evento, categoria, preco, quantidade_disponivel } = req.body;
+        const newIngresso = await ingressoModel.createIngresso(evento, local, data_evento, categoria, preco, quantidade_disponivel);
+        res.status(201).json(newIngresso);
+    } catch (error) {
+        console.log(error);
+        if (error.code === "23505") { 
+            return res.status(400).json({ message: " Ingresso já cadastrado." });
+        }
+        res.status(500).json({ message: "Erro ao criar ingresso." });
+    }
+};
+
+const updateIngresso = async (req, res) => {
+    try {
+        const { evento, local, data_evento, categoria, preco, quantidade_disponivel } = req.body;
+        const updateIngresso = await ingressoModel.updateIngresso(req.params.id, evento, local, data_evento, categoria, preco, quantidade_disponivel);
+        if (!updateIngresso) {
+            return res.status(404).json({ message: "Ingresso não encontrado." });
+        }
+        res.json(updateIngresso);
+    } catch (error) {
+        res.status(500).json({ message: "Erro ao atualizar ingresso." });
+    }
+};
+
+const deleteIngresso = async (req, res) => {
+    try {
+        const message = await ingressoModel.deleteIngresso(req.params.id);
+        res.json(message);
+    } catch (error) {
+        res.status(500).json({ message: "Erro ao deletar ingresso." });
+    }
+};
+
+module.exports = { getAllIngressos, getIngresso, createIngresso, updateIngresso, deleteIngresso };
+
